9c2cbed494ae345a1b7b18b84e0fb33502a9033c
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-06-13 18:12:21 +0300

    first commit

---
+++ challemge1.dart
@@ -0,0 +1,45 @@
+/// TODO : challenge 1
+
+void main() {
+  /// fix syntax
+
+  /// Q1:
+
+  /// int x = 'Mohamed';
+
+  /// double y = true;
+  /// bool x = 5;
+  /// String result = 4.4;
+
+  /// solution
+  
+  String name = 'Mohamed';
+  bool y = true;
+  int x = 5;
+  double result = 4.4;
+
+  /// Q2:
+
+  /// String name = 'Mohamed', int age =30;
+  /// print('name : name , age : #age');
+  /// output (name : Mohamed, age 30)
+
+  /// solution
+  
+   String name1 = 'Mohamed';
+   int age =30;
+   print('name : $name1 , age : $age');
+//    output (name : Mohamed, age : 30);
+
+  /// Q3:
+
+  /// int n = 10;
+  /// if(n >= 5) print('Greater than or Equal 5');
+  /// else print(less than  5')
+
+  /// solution
+  int n = 10;
+  if(n >= 5) print('Greater than or Equal 5');
+  else print('less than 5');
+  
+}
